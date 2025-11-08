d788394149190d3648a12581fd6a7b7cb7a43a07
Dror Granot <dror091@gmail.com>
2025-11-08 10:54:41 +0200

    fix: use https stage api endpoint

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://amesa-backend-stage-alb-467028641.eu-north-1.elb.amazonaws.com',
-  backendUrl: 'http://amesa-backend-stage-alb-467028641.eu-north-1.elb.amazonaws.com',
+  apiUrl: 'https://d2ejqzjfslo5hs.cloudfront.net/api/v1',
+  backendUrl: 'https://d2ejqzjfslo5hs.cloudfront.net',
   frontendUrl: 'https://d2ejqzjfslo5hs.cloudfront.net',
   logLevel: 'warn'
 };
