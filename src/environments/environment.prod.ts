c9cfed648a98e5f5bd49b4691e57f7f0b9ba594f
Dror Granot <dror091@gmail.com>
2025-11-19 23:12:47 +0200

    fix: Change backend URL from HTTP to HTTPS in production
    - Fixes Mixed Content error (HTTPS page requesting HTTP resources)
    - Browsers block mixed content for security
    - Production frontend now uses HTTPS for all backend API calls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://amesa-backend-alb-509078867.eu-north-1.elb.amazonaws.com/api/v1',
-  backendUrl: 'http://amesa-backend-alb-509078867.eu-north-1.elb.amazonaws.com/api/v1',
+  apiUrl: 'https://amesa-backend-alb-509078867.eu-north-1.elb.amazonaws.com/api/v1',
+  backendUrl: 'https://amesa-backend-alb-509078867.eu-north-1.elb.amazonaws.com/api/v1',
   frontendUrl: 'https://dpqbvdgnenckf.cloudfront.net',
   logLevel: 'error'
 };
